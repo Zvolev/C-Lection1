7b60b7bfd7d17bc747df53510b6d2bb6c821b62f
Evgenii <zvolev@mail.ru>
2022-10-03 17:59:07 +0300

    привет с условием

---
+++ Ext5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Здрасте, как вас зовут? ");
+string nameuser = Console.ReadLine();
+
+if (nameuser.ToLower() == "маша")
+{
+    Console.Write("Привет, Мария!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(nameuser);
+}
